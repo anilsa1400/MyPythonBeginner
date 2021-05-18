2c750e8980c4b0f559389099a8f3803f754feadb
Anil <anilsa1400@gmail.com>
2021-05-18 20:48:40 +0530

    ch4

--- Ch2/classes_start.py
+++ Ch2/classes_start.py
@@ -2,8 +2,31 @@
 # Example file for working with classes
 #
 
+class myClass():
+    def method1(self):
+        print("myClass method1")
+
+    def method2(self, someString):
+        print("myClass method2 "+someString)
+
+
+class anotherClass(myClass):
+    def method1(self):
+        myClass.method1(self)
+        print("another method1")
+
+    def method2(self, someString):
+        print("another method2 "+someString)
+
 
 def main():
+    c = myClass()
+    c.method1()
+    c.method2("This is a string")
+
+    c2 = anotherClass()
+    c2.method1()
+    c2.method2("This is a another string")
 
 
 if __name__ == "__main__":
